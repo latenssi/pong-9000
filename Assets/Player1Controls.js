2c371d65cbc7db92273869933f9a2d1226ecc6fc
Elunthir <chuang.li@hotmail.fi>
2016-01-18 17:46:56 +0200

    Name Change
    rename test1 to game secene

---
+++ Assets/Player1Controls.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+    if (Input.GetKey ("up")) 
+        {
+        gameObject.y++;
+        }
+
+    if (Input.GetKey ("down")) 
+        {
+        gameObject.y--;
+        }
+
+    if (Input.GetKey ("left")) 
+        {
+        gameObject.x--;
+        }
+
+    if (Input.GetKey ("right")) 
+        {
+        gameObject.x++;
+        }
+}
