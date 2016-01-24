c784f8ed0f08b66f91519883a98ab933e8635448
Lauri Junkkari <latenssi@gmail.com>
2016-01-25 00:25:17 +0200

    Refactor everything.

--- Assets/RotateBall.js
+++
@@ -1,8 +0,0 @@
-﻿#pragma strict
-public var speed : int = 200;
-
-
-function Update () {
-    transform.Rotate(Vector3.right * Time.deltaTime * speed);
-    transform.Rotate(Vector3.up * Time.deltaTime * speed);
-}
---
+++ Assets/Scripts/PaddleMovement.js
@@ -0,0 +1,41 @@
+﻿#pragma strict
+
+public static var DISCRETE_UP : float = 1;
+public static var DISCRETE_DOWN : float = -1;
+public static var DISCRETE_LEFT : float = -1;
+public static var DISCRETE_RIGHT : float = 1;
+public static var DISCRETE_STOP  : float = 0;
+
+private var speed : int;
+private var rb : Rigidbody;
+private var vertical : int;
+private var horizontal : int;
+
+function Start() {
+    speed = 20;
+    vertical = 0;
+    horizontal = 0;
+    rb = GetComponent(Rigidbody);
+}
+
+
+function FixedUpdate() {
+    rb.velocity.y = vertical * speed;
+    rb.velocity.x = horizontal * speed;
+}
+
+function setSpeed(newSpeed : int) {
+    speed = newSpeed;
+}
+
+function getSpeed() {
+    return speed;
+}
+
+function moveVertical(direction : float) {
+    vertical = direction;
+}
+
+function moveHorizontal(direction : float) {
+    horizontal = direction;
+}
