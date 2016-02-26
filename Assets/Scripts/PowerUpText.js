d8cc47ee60c7eed38c39906186d71554efbab36a
Lauri Junkkari <latenssi@gmail.com>
2016-02-26 19:13:45 +0200

    Add power up texts

---
+++ Assets/Scripts/PowerUpText.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+var powerupText : String;
+var textObject : GameObject;
+
+function OnCollisionEnter (collision: Collision) {
+  var newText = Instantiate(textObject, gameObject.transform.position, Quaternion.identity);
+  newText.GetComponent(TextMesh).text = powerupText;
+}
---
+++ Assets/Scripts/PowerUpTextTimer.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var duration : int;
+
+function Start () {
+  Destroy(gameObject, duration);
+}
+
+function FixedUpdate() {
+  gameObject.transform.position.y += 0.05;
+  gameObject.transform.GetComponent(Renderer).material.color.a -= 0.01;
+}
